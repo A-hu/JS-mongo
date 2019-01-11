c91b3cdd226f26fcb5a0f2fb9bac0840eada8157
A-hu <louis0911317399@gmail.com>
2019-01-12 01:18:38 +0800

    Schemas

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const mongoDB = 'mongodb://127.0.0.1/playground';
 mongoose.connect(mongoDB, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err))
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tag: [ String ],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
