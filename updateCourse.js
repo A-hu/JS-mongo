651febda2d87f198fdbea16adb3f07c5f4a15da9
A-hu <louis0911317399@gmail.com>
2019-01-15 11:02:33 +0800

    Updating a Document - Update First

--- updateCourse.js
+++ updateCourse.js
@@ -3,19 +3,28 @@ const Course = require('./courseClass')
 async function updateCourse(id) {
   // Approach: Query first
   // findById()
-  const course = await Course.findById(id);
-  if(!course) return;
+  // const course = await Course.findById(id);
+  //
+  // https://docs.mongodb.com/manual/reference/operator/update/
+  // const result = await Course.update({ _id: id }, {
+  const course = await Course.findByIdAndUpdate({ _id: id }, {
+    $set: {
+      author: 'Jason',
+      isPublished: false
+    }
+  }, { new: true });
+  // if(!course) return;
 
   // Modify its properties
-  course.isPublished = true;
-  course.author = 'Another Author';
+  // course.isPublished = true;
+  // course.author = 'Another Author';
   // course.set({
   //   isPublished: true,
   //   author: 'Another Auther'
   // });
   // save()
-  const result = await course.save();
-  console.log(result);
+  // const result = await course.save();
+  console.log(course);
   //
   // Approach: Update first
   // Update directly
